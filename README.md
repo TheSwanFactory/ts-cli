b4383025eb80107e6307647a5177357988bc4dd9
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2023-09-15 21:53:23 -0700

    3 testing (#4)
    * ncu
    
    * add mocha chai to dev
    
    * Update README.md
    
    * npm test
    
    * Add node GitHub Action
    
    * helper method `run`
    
    * check action pwd
    
    * clean build -> dist/src
    
    * test age

--- README.md
+++ README.md
@@ -4,27 +4,29 @@ Demo project to create/test CLIs written using TypeScript
 It is surprisingly difficult to find a currently-working example of how
 to create and test Command-Line Interfaces (CLIs) written in TypeScript.
 
-# Purpose
-
 This project updates the
 [How to Create a Testable CLI using TypeScript?](https://www.realpythonproject.com/how-to-create-a-testable-cli-using-typescript/)
 from Jan 2023 with various fixes that were not obvious (at least to me).
 
 These include:
 
-*  Restricting to "chalk": "^4.1.2"
+* Restricting "chalk" to "^4.1.2"
+* Avoiding the confusingly-named 'npm run install'
+* import { expect } from "chai";
+* "test" : "mocha --require ts-node/register test/**/*.ts"
+* Add node GitHub Action (tests dist/src/index.js, not installed version) 
 
-## Functionality
 
-Use `npm run` to see all commands:
+## Usage
 
 ```bash
-$ npm run hello
+$ npm run ts-cli-install
+$ ts-cli hello
 Hello, World!
-$ npm run hello Universe!
+$ ts-cli hello Universe!
 Hello, Universe!!
-$ npm run calc -o multiply 10 16 # or: add | divide
+$ ts-cli calc -o multiply 10 16 # or: add | divide
 160
-$ npm run age Ernie # Use axios to call the agify API 
+$ ts-cli age Ernie # Use axios to call the agify API 
 Ernie - 74
 ```
---
+++ test/cli.spec.ts
@@ -0,0 +1,42 @@
+import { test } from "mocha";
+import { expect } from "chai";
+import * as childProcess from "child_process";
+
+// helper method to call execSync with dist/index.js
+const run = (args: string) => {
+    return childProcess.execSync(`node ./dist/src/index.js ${args}`, {
+        encoding: "utf8",
+    });
+};
+
+test("should print hello world", () => {
+    // Run the hello command and capture the output
+    const output = run(`hello`);
+
+    // Check that the output is correct
+    expect(output).to.equal("Hello, World!\n");
+});
+
+test("should print a greeting", () => {
+    // Run the hello command and capture the output
+    const output = run(`hello Rahul`);
+
+    // Check that the output is correct
+    expect(output).to.equal("Hello, Rahul!\n");
+});
+
+test("should perform the specified operation", () => {
+    // Run the calculate command and capture the output
+    const output = run(`calc 3 4 -o multiply`);
+
+    // Check that the output is correct
+    expect(output).to.equal("12\n");
+});
+
+test("should print the typical age for a given name", () => {
+    // Run the age command and capture the output
+    const output = run(`age Rahul`);
+
+    // Check that the output is correct
+    expect(output).to.equal("Rahul - 40\n");
+});
