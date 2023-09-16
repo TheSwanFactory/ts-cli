55a1d2fb3bc2da94ad52e38230e986febdc7b552
Dr. Ernie Prabhakar <ernest.prabhakar@gmail.com>
2023-09-16 13:51:13 -0700

    7 install cli (be)for testing (#8)
    * How to use ESM for a TypeScript CLI
    
    * install for test
    
    * improve comments
    
    * test-log

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+// NOTE - using ESM modules requilres explicit '.js' extension
+// even when importing from a TypeScript file
+
 import { getAge, performOperation, printName, } from "./utils.js";
 import { Command } from "commander";
 
@@ -18,7 +21,7 @@ program
     .command("calc")
     .argument("<num1>")
     .argument("<num2>")
-    .option("-o, --operation <operation>", "Operation to perform: add, multiply, divide")
+    .option("-o, --operation <operation>", "Operation to perform: add | multiply | divide")
     .action(performOperation);
 
 program.parse(process.argv);
--- test/cli.spec.ts
+++ test/cli.spec.ts
@@ -2,41 +2,41 @@ import { test } from "mocha";
 import { expect } from "chai";
 import * as childProcess from "child_process";
 
-// helper method to call execSync with dist/index.js
-const run = (args: string) => {
-    return childProcess.execSync(`node ./dist/src/index.js ${args}`, {
-        encoding: "utf8",
-    });
+// helper method to run command in shell
+const exec = (command: string) => {
+    return childProcess.execSync(command, {encoding: "utf8"});
 };
 
-test("should print hello world", () => {
-    // Run the hello command and capture the output
-    const output = run(`hello`);
+const run = (args: string) => {
+    return exec(`ts-cli ${args}`);
+};
 
-    // Check that the output is correct
+before(() => {
+    exec(`npm install -g`);
+    console.log("Installed CLI");
+});
+
+after(() => {
+    exec(`npm uninstall -g`);
+    console.log("Uninstalled CLI");
+});
+
+test("should print hello world", () => {
+    const output = run(`hello`);
     expect(output).to.equal("Hello, World!\n");
 });
 
 test("should print a greeting", () => {
-    // Run the hello command and capture the output
     const output = run(`hello Rahul`);
-
-    // Check that the output is correct
     expect(output).to.equal("Hello, Rahul!\n");
 });
 
 test("should perform the specified operation", () => {
-    // Run the calculate command and capture the output
     const output = run(`calc 3 4 -o multiply`);
-
-    // Check that the output is correct
     expect(output).to.equal("12\n");
 });
 
 test("should print the typical age for a given name", () => {
-    // Run the age command and capture the output
     const output = run(`age Rahul`);
-
-    // Check that the output is correct
     expect(output).to.equal("Rahul - 40\n");
 });
